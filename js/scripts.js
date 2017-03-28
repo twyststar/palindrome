e8eb43a940f2937cd7f17f64dc9ff4a33e0ddedd
Elan Baharie, Starlynne Start and Zach Bastian <starlynne@>
2017-03-28 15:20:02 -0700

    it will add input form, palindrome checker.

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,19 @@ $(function(){
   $('#formOne').submit(function(event) {
     event.preventDefault();
 
-    var user1 = parseInt($('input#play1').val());
-    var user2 = parseInt($('input#play2').val());
+    var user1 = $('input#play1').val();
+    var user2 = user1.replace(/[ ]/g, "");
+    var user3 = user2.split("");
+    var user4 = user3.slice();
+    user4.reverse();
 
-    var numberOfTimes = (user2 / user1);
-
-    console.log(numberOfTimes);
-    // console.log(user2);
-    if (numberOfTimes <= 0) {
-      alert("This will not compute.");
+    console.log();
+    console.log();
+    if (user3.toString() === user4.toString()) {
+      alert("palindrome!");
+    } else {
+      alert("Nope!")
     }
 
-    if (isNaN(user1) || isNaN(user2))  {
-        alert('great!');}
-
-    var total = [];
-
-
-
-    if ((user1 > 0 ) && (user2 > 0)) {
-      for (var index = user1; index <= user2; index += user1 ) {
-        total.push(index);
-      }
-    } else if ((user1 < 0 ) && (user2 < 0)) {
-      for (var index = user1; index >= user2; index += user1 ) {
-        total.push(index);
-      }
-    }
-    alert(total);
   });
 });
